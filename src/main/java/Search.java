9eb98c64aeb97b7dc8b2e7188eb5600d1f565384
akahramn <a.kahramnn@gmail.com>
2024-08-05 02:03:09 +0300

    adding database connection

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -5,14 +5,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet("/search")
 public class Search extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String keyword = request.getParameter("keyword");
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h3>This is keyword: </h3> " + keyword + "<br>");
+        System.out.println(keyword);
+        try{
+            Connection connection = DatabaseConnection.getConnection();
+            //add keyword into history table
+            PreparedStatement preparedStatement = connection.prepareStatement("Insert into history values(?, ?)");
+            preparedStatement.setString(1, keyword);
+            preparedStatement.setString(2, "http://localhost:8080/SearchEngineAccio/Search?keyword="+keyword);
+            preparedStatement.executeUpdate();
+
+            //get results from pages table
+            ResultSet resultSet = connection.createStatement().executeQuery("select pageTitle, pageLink, (length(lower(pageData))-length(replace(lower(pageData), '" + keyword + "', \"\")))/length('" + keyword + "') as countoccurence from pages order by countoccurence desc limit 30;");
+            ArrayList<SearchResult> results = new ArrayList<SearchResult>();
+            while (resultSet.next()){
+                SearchResult searchResult = new SearchResult();
+                searchResult.setPageTitle(resultSet.getString("pageTitle"));
+                searchResult.setPageLink(resultSet.getString("pageLink"));
+                results.add(searchResult);
+            }
+            for(SearchResult result:results){
+                System.out.println(result.getPageLink()+" "+result.getPageTitle()+"\n");
+            }
+            request.setAttribute("results", results);
+            request.getRequestDispatcher("/search.jsp").forward(request, response);
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+        } catch (SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+        catch (ServletException servletException){
+            servletException.printStackTrace();
+        }
+        catch (IOException ioException){
+            ioException.printStackTrace();
+        }
     }
 }
