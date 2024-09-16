bee6152102f4c41f831bb3d3096f091cd305f96c
akahramn <a.kahramnn@gmail.com>
2024-09-16 23:55:44 +0300

    .

--- search-engine/src/main/java/com/abdullahkahraman/search_engine/page/PageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abdullahkahraman.search_engine.page;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PageRepository extends JpaRepository<Page, Long> {
-}
