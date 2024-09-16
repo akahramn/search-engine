128cf2a36fd6e4264829692d926bf3725a048992
akahramn <a.kahramnn@gmail.com>
2024-09-17 01:57:32 +0300

    search engine connect kafka consumer

---
+++ search-engine/src/main/java/com/abdullahkahraman/search_engine/service/PageService.java
@@ -0,0 +1,13 @@
+package com.abdullahkahraman.search_engine.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PageService {
+
+    @KafkaListener(topics = "crawler-operation")
+    public void listenGroupFoo(String message) {
+        System.out.println("Received Message in group foo: " + message);
+    }
+}
