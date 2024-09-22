801a4018beb2296971d06aa028399026de619505
akahramn <a.kahramnn@gmail.com>
2024-09-22 15:31:22 +0300

    outbox removed

--- web-crawler/src/main/java/com/abdullahkahraman/web_crawler/repository/OutboxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abdullahkahraman.web_crawler.repository;
-
-import com.abdullahkahraman.web_crawler.model.Outbox;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OutboxRepository extends JpaRepository<Outbox, Long> {
-}
