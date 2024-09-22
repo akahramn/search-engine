f0cb1d4ac179f73bf672f3dbd79c0c60b8a7a460
akahramn <a.kahramnn@gmail.com>
2024-09-22 16:17:57 +0300

    el changes adding

--- search-engine/src/main/java/com/abdullahkahraman/search_engine/config/ElasticsearchConfig.java
+++ search-engine/src/main/java/com/abdullahkahraman/search_engine/config/ElasticsearchConfig.java
@@ -1,24 +1,33 @@
 package com.abdullahkahraman.search_engine.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.abdullahkahraman.search_engine.repository")
-@ComponentScan(basePackages = "com.abdullahkahraman.search_engine")
-public class ElasticsearchConfig extends ElasticsearchConfiguration {
+public class ElasticsearchConfig {
 
     @Value("${elasticsearch.url}")
-    private String url;
+    private String serverUrl;
 
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(url)
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        // Create the low-level client
+        RestClient restClient = RestClient
+                .builder(HttpHost.create(serverUrl))
                 .build();
+
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+       return new ElasticsearchClient(transport);
     }
 }
